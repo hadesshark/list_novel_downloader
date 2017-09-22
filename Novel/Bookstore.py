593f1b1c0c3f2101c057c515c6f07bf3aefe1103
王思尹 <hades_shark@hotmail.com>
2017-09-22 20:36:17 +0800

    add : 寫一個 Novel 模組

---
+++ Novel/Bookstore.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+def get_book(book):
+    pass
+
+
+def update():
+    pass
+
+
+def check_have_book(book):
+    pass
+
+
+def get_books_menu():
+    pass
+
+
+def add_book(book):
+    pass
---
+++ Novel/Content.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+def get_chapter(chapter_number):
+    pass
+
+
+def save():
+    pass
